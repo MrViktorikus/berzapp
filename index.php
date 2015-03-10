ec015e8f05f152746fb42d841a3bdd3247f52777
albsjo969 <albsjo969@E29648.edu.linkoping.se>
2015-03-10 13:22:16 +0100

    Lagt till login (med kill) och kundvagn. Har inte ändrat namen än. //2014-12-01 10:11  ¤CaptainBalbin¤
    Lagt till databas med tillhörande css. Ändrade namen till passande namn, dvs databasen har namnet db. Dock vet jag ej vad index ska vara. Just nu är kunvagnen index. Kan diskuteras. // 2014-12-01 10:15 ¤CaptainBalbin¤
    
    Har nu länkat med databasen i Github så att det inte behövs göras ny db varje gång. // 2014-12-01 10:27 ¤CaptainBalbin¤
    
    Bytt namn på databasen samt har bara 1 databas för både login och products, i olka listor. // 2014-12-01 10:43

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
             foreach ($infos as $info) {
                 echo "<div class='nils'>";
                 echo "<h1>" . $info["rubrik"] . "</h1>" . "<br>";
-                echo "<img src='img/" . $info["filnamn"] . "' alt='Corrupt File' width='100'>" . "<br>";
-                echo $info["flode"] . "<br>";
+                echo "<img src='img/" . $info["filnamn"] . "' alt='Corrupt File' width='350'>" . "<br>";
+                echo "<p>" . $info["flode"] . "</p>" . "<br>";
                 echo $info["tid"] . "<br>";
                 echo "<form method='GET'>";
                 echo "<input type='hidden' value='" . $info["id"] . "' name='id'>";
